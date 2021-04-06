906c097aa24f2d426097cbd3e78d92d20ca03c66
Mayank Tolani <mayanktolani19@gmail.com>
2021-04-06 21:18:44 +0530

    Initial commit

---
+++ DecisionTree.py
@@ -0,0 +1,54 @@
+# Decision Tree Classifier
+
+# Import the necessary Libraries
+import pandas as pd
+import time
+
+# For text feature extraction
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# For creating a pipeline
+from sklearn.pipeline import Pipeline
+
+# Classifier Model (Decision Tree)
+from sklearn.tree import DecisionTreeClassifier
+
+# To save the trained model on local storage
+import joblib
+
+start_time = time.time()
+# Read the File
+data = pd.read_csv('data.csv')
+
+# Features which are passwords
+features = data.values[:, 1].astype('str')
+
+# Labels which are strength of password
+labels = data.values[:, -1].astype('int')
+
+# Splitting the dataset into the training and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test=train_test_split(features,labels,test_size=0.25,random_state=0)
+
+# Sequentially apply a list of transforms and a final estimator
+classifier_model = Pipeline([
+                ('tfidf', TfidfVectorizer(analyzer='char')),
+                ('decisionTree',DecisionTreeClassifier()),
+])
+
+# Fit the Model
+classifier_model.fit(X_train, y_train)
+
+y_pred = classifier_model.predict(X_test)
+
+from sklearn.metrics import confusion_matrix, classification_report
+cm=confusion_matrix(y_test,y_pred)
+print(classification_report(y_test, y_pred, digits=4))
+print("Confusion Matrix: \n", cm)
+accuracy = (cm[0][0]+cm[1][1]+cm[2][2])/(cm[0][0]+cm[0][1]+cm[0][2]+cm[1][0]+cm[1][1]+cm[1][2]+cm[2][0]+cm[2][1]+cm[2][2])
+print('Training Accuracy: ',classifier_model.score(features, labels))
+print("Testing Accuracy = ", accuracy)
+print("Time Taken to train the model = %s seconds" % round((time.time() - start_time),2))
+# Save model
+joblib.dump(classifier_model, 'DecisionTree_Model.joblib')
+
---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, flash, request
+import joblib as joblib
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/main/', methods=['GET', 'POST'])
+def mainpage():
+    if request.method == "POST":
+        enteredPassword = request.form['password']
+    else:
+        return render_template('index.html')
+
+    # Load the algorithm models
+    DecisionTree_Model = joblib.load('DecisionTree_Model.joblib')
+    print("Decision Tree Loaded")
+    LogisticRegression_Model = joblib.load('LogisticRegression_Model.joblib')
+    NaiveBayes_Model = joblib.load('NaiveBayes_Model.joblib')
+    RandomForest_Model = joblib.load('RandomForest_Model.joblib')
+    NeuralNetwork_Model = joblib.load('NeuralNetwork_Model.joblib')
+    
+    Password = [enteredPassword]
+
+    # Predict the strength
+    DecisionTree_Test = DecisionTree_Model.predict(Password)
+    LogisticRegression_Test = LogisticRegression_Model.predict(Password)
+    NaiveBayes_Test = NaiveBayes_Model.predict(Password)
+    RandomForest_Test = RandomForest_Model.predict(Password)
+    NeuralNetwork_Test = NeuralNetwork_Model.predict(Password)
+    
+
+    return render_template("main.html", DecisionTree=DecisionTree_Test,
+                                        LogReg=LogisticRegression_Test, 
+                                        NaiveBayes=NaiveBayes_Test,
+                                        RandomForest=RandomForest_Test,
+                                        NeuralNetwork=NeuralNetwork_Test
+                                        )
+
+if __name__ == "__main__":
+    app.run()
