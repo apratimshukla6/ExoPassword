5339e5645708b0055d99161ceccafedaedcb1f4d
Mayank Tolani <mayanktolani19@gmail.com>
2021-04-07 21:31:35 +0530

    Added data file

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def mainpage():
     print("Decision Tree Loaded")
     LogisticRegression_Model = joblib.load('LogisticRegression_Model.joblib')
     NaiveBayes_Model = joblib.load('NaiveBayes_Model.joblib')
-    RandomForest_Model = joblib.load('RandomForest_Model.joblib')
+    # RandomForest_Model = joblib.load('RandomForest_Model.joblib')
     NeuralNetwork_Model = joblib.load('NeuralNetwork_Model.joblib')
     
     Password = [enteredPassword]
@@ -29,14 +29,14 @@ def mainpage():
     DecisionTree_Test = DecisionTree_Model.predict(Password)
     LogisticRegression_Test = LogisticRegression_Model.predict(Password)
     NaiveBayes_Test = NaiveBayes_Model.predict(Password)
-    RandomForest_Test = RandomForest_Model.predict(Password)
+    # RandomForest_Test = RandomForest_Model.predict(Password)
     NeuralNetwork_Test = NeuralNetwork_Model.predict(Password)
     
 
     return render_template("main.html", DecisionTree=DecisionTree_Test,
                                         LogReg=LogisticRegression_Test, 
                                         NaiveBayes=NaiveBayes_Test,
-                                        RandomForest=RandomForest_Test,
+                                        RandomForest=0,
                                         NeuralNetwork=NeuralNetwork_Test
                                         )
 
