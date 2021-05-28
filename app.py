357b77d24f56ac0793854c79241be89b89f95d8f
Apratim Shukla <apratimshukla6@gmail.com>
2021-05-28 21:17:15 +0530

    Additional Features

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from typing import final
 from flask import Flask, render_template, flash, request
 import joblib as joblib
 import os
@@ -12,13 +11,12 @@ port = int(os.environ.get('PORT', 5000))
 app.config['SECRET_KEY'] = 'abc123@567$#'
 
 def callEnzoicAPI(password):
-    apiKey = ''
-    secretKey = ''
+    apiKey = 'f8e26dc0edbf441080becff0719deff1'
+    secretKey = 'S7Th9YvpKzRnwAp4Cd!mg^P&?MMc7!Eq'
     authorizationParameter = apiKey+":"+secretKey
     authorizationParameter = authorizationParameter.encode('ascii')
     authorizationParameter = (base64.b64encode(authorizationParameter)).decode('ascii')
     authorizationParameter = "basic "+str(authorizationParameter)
-    print(authorizationParameter)
     password = password.encode('utf-8')
     sha1HashTemp = hashlib.sha1(password)
     sha1Hash = str(sha1HashTemp.hexdigest())
@@ -28,20 +26,66 @@ def callEnzoicAPI(password):
     md5Hash = str(md5HashTemp.hexdigest())
     rawData = {'partialSHA1':sha1Hash, 'partialSHA256':sha256Hash, 'partialMD5':md5Hash}
     url = 'https://api.enzoic.com/passwords'
-    #authorization parameter is basic BASE64(APIKEY:SECRETKEY)
     response = requests.post(url, data = json.dumps(rawData),headers={'content-type':'application/json',
     'authorization':authorizationParameter})
     if(response.status_code == 404):
         print("Not found")
-        return False,0
-    finalResponse = response.conten.decode('ascii')
-    return finalResponse["candidates"][0]["revealedInExposure"], finalResponse["candidates"][0]["exposureCount"]
+        return (False,0)
+    finalResponse = json.loads(response.content.decode('ascii'))
+    return (finalResponse["candidates"][0]["revealedInExposure"], finalResponse["candidates"][0]["exposureCount"])
+
+
+def scoreCalculate(password):
+    length = len(password)
+    score = 0
+    upper = 0
+    lower = 0
+    numbers = 0
+    symbols = 0
+    consecutiveLower = 0
+    consecutiveUpper = 0
+    consecutiveNumber = 0
+    for i in range(0,length-1):
+        if(password[i].isupper()):
+            upper += 1
+        elif(password[i].islower()):
+            lower += 1
+        elif(password[i].isdigit()):
+            numbers += 1
+        else:
+            symbols += 1
+        
+        if(password[i].isupper() and password[i+1].isupper()):
+            consecutiveUpper += 1
+        else:
+            score = score-consecutiveUpper*2
+            consecutiveUpper = 0
+        if(password[i].islower() and password[i+1].islower()):
+            consecutiveLower += 1
+        else:
+            score = score-consecutiveLower*2
+            consecutiveLower = 0
+        if(password[i].isdigit() and password[i+1].isdigit()):
+            consecutiveNumber += 1
+        else:
+            score = score-consecutiveNumber*2
+            consecutiveNumber = 0
+
+
+    score = score + length*4+((length-lower)*2)+((length-upper)*2)+(numbers*4)+symbols*6
+
+    if(numbers==0 and symbols==0):
+        score = score - length
+    
+    if(upper==0 and lower==0 and symbols==0):
+        score = score - length
+    
+    return score
+
 
 
 @app.route('/')
 def homepage():
-    hi = callEnzoicAPI("EarthingIsNoob123")
-    print(hi[0], hi[1])
     return render_template('index.html')
 
 
@@ -54,7 +98,6 @@ def mainpage():
 
     # Load the algorithm models
     DecisionTree_Model = joblib.load('DecisionTree_Model.joblib')
-    print("Decision Tree Loaded")
     LogisticRegression_Model = joblib.load('LogisticRegression_Model.joblib')
     NaiveBayes_Model = joblib.load('NaiveBayes_Model.joblib')
     NeuralNetwork_Model = joblib.load('NeuralNetwork_Model.joblib')
@@ -66,13 +109,16 @@ def mainpage():
     LogisticRegression_Test = LogisticRegression_Model.predict(Password)
     NaiveBayes_Test = NaiveBayes_Model.predict(Password)
     NeuralNetwork_Test = NeuralNetwork_Model.predict(Password)
-    apiResult = callEnzoicAPI(Password)
+    status, count = callEnzoicAPI(enteredPassword)
+    score = scoreCalculate(enteredPassword)
 
     return render_template("main.html", DecisionTree=DecisionTree_Test,
                                         LogReg=LogisticRegression_Test, 
                                         NaiveBayes=NaiveBayes_Test,
-                                        NeuralNetwork=NeuralNetwork_Test
-                                        )
+                                        NeuralNetwork=NeuralNetwork_Test,
+                                        status=status,
+                                        count=count,
+                                        score=score)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
